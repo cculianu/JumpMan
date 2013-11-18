daa61be3267820d593d9455bb90abb8096940605
maijavilkina <maija.vilkina@gmail.com>
2013-11-18 14:45:07 +0100

    graphics

---
+++ test01/Debug/helloworld.d
@@ -0,0 +1,1 @@
+helloworld.d: ../helloworld.cc
---
+++ test01/helloworld.cc
@@ -0,0 +1,16 @@
+/*
+ * helloworld.cc
+
+ *
+ *  Created on: Nov 13, 2013
+ *      Author: maijav
+ */
+#include <iostream>
+using namespace std;
+
+int main() {
+	string silly = "Maija!";
+	cout << "Hello " << silly << endl;
+	getline(cin, silly);
+	return 0;
+}
