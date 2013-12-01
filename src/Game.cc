ce2e313580e0812131f5610fdec5493251b22450
Olle K <olle.kvarnstroem@gmail.com>
2013-12-02 00:37:58 +0100

    added doxy, changed screen width to 1000, changed fps to 24 and the objects now scroll

--- src/Game.cc
+++ src/Game.cc
@@ -1,10 +1,8 @@
 #include <iostream>
 #include <string>
-#include <list>
+#include <algorithm>
 
 #include "Game.hh"
-
-#include "BasicEnemy.hh"
 
 using namespace std;
 
@@ -13,10 +11,10 @@ Game::Game() :
 {
   /* Start graphics: */
   const string TITLE = "Jumpman";
-  const unsigned SCREEN_WIDTH = 600;
+  const unsigned SCREEN_WIDTH = 1000;
   const unsigned SCREEN_HEIGHT = 600;
   const unsigned SCREEN_BPP = 32;
-  const unsigned FRAME_RATE = 20;
+  const unsigned FRAME_RATE = 24;
   graphics_= new GraphicsEngine(TITLE, SCREEN_WIDTH, SCREEN_HEIGHT,
                                 SCREEN_BPP, FRAME_RATE);
 
@@ -28,7 +26,6 @@ Game::Game() :
     std::cerr << "Failed to load image: " << graphics_->getLastError() << '\n';
     exit(1);
   }
-
 }
 
 Game::~Game()
@@ -38,17 +35,10 @@ Game::~Game()
 
 int Game::run()
 {
-  Player player("player", 0, 0, 40, 40);
+  Player player("player", 0, 0, 20, 20);
 
   list<BasicEnemy> enemies;
-  size_t current_highest_enemy;
-  enemies.push_back(BasicEnemy("jumpy", player, 40, 40));
-  do
-  {
-    enemies.push_back(BasicEnemy("jumpy", enemies.back(), 40, 40));
-    current_highest_enemy = enemies.back().y();
-  }
-  while (current_highest_enemy < graphics_->screen_height());
+  enemies.push_back(BasicEnemy("jumpy", player, 20, 20, graphics_->screen_width()/2));
 
   rect_t draw_to;
 
@@ -69,7 +59,18 @@ int Game::run()
 
     /* Let the objects move and interact with each other */
     player.handleGravity(static_cast<signed>(graphics_->screen_width()));
-    enemies.remove_if([&player](BasicEnemy enemy) { return player.touches(enemy); });
+    enemies.remove_if([&player](BasicEnemy enemy) 
+        { return player.touches(enemy) || enemy.y() < 0; });
+    addEnemies(enemies);
+
+    /* If player is above the middle of the screen, lower everything */
+    int offset_y = player.y() - graphics_->screen_height()/2;
+    if (offset_y > 0)
+    {
+      player.modifyY(-offset_y);
+      for_each(enemies.begin(), enemies.end(), [offset_y](BasicEnemy &enemy)
+          { enemy.modifyY(-offset_y); });
+    }
 
     /* Draw everything to screen */
     graphics_->drawImage("background", NULL, NULL);
@@ -89,4 +90,18 @@ int Game::run()
 
   /* END NOT REACHED */
   return 1;
+}
+
+int Game::addEnemies(list<BasicEnemy> &enemies)
+{
+  int counter = 0;
+  signed screen_height = static_cast<signed>(graphics_->screen_height());
+
+  while (enemies.back().y() < screen_height)
+  {
+    enemies.push_back(BasicEnemy("jumpy", enemies.back(), 20, 20,
+                      graphics_->screen_width()/2));
+    ++counter;
+  }
+  return counter;
 }
--- src/Game.hh
+++ src/Game.hh
@@ -1,25 +1,60 @@
 #ifndef __GAME_HH__
 #define __GAME_HH__
 
-#include <string>
+/*! 
+ * \file Game.hh
+ * \brief File containing the Game class Header
+ *
+ * \author Olle Kvarnström
+ * \date
+ */
+
+#include <list>
 
 #include "GraphicsEngine.hh"
 #include "Sprite.hh"
 #include "Player.hh"
+#include "BasicEnemy.hh"
 
+/*! 
+ * \class Game
+ *
+ * \brief Main game class
+ *
+ * Game is the main class, which acts as the core of the application
+ * It's role is glue the other projects together in a structured way
+ * without doing any work by itself 
+ */
 class Game
 {
   public:
+
+    ///Constructor
     Game();
+
+    ///Destructor
     ~Game();
 
+    /*!
+     * \brief The game's main loop
+     * \return 0 on success
+     */
     int run();
 
   private:
+    /*!
+     * \brief Add enemies until they fill up the screen 
+     * \param enemies list of enemies in run()-function
+     */
+    int addEnemies(std::list<BasicEnemy> &enemies);
+
+    ///Instance for managing graphics
     GraphicsEngine *graphics_;
 
-    /* These cannot be used: */
+    ///Disabled copy constructor
     Game(const Game&);
+
+    ///Disabled copy constructor
     void operator=(const Game&);
 
 };
