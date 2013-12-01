987b6402b1d64b75bb93fec652753eeaac15d45e
Olle K <olle.kvarnstroem@gmail.com>
2013-12-01 19:45:37 +0100

    Made a copy constructor, and made width/height const

--- src/Sprite.cc
+++ src/Sprite.cc
@@ -1,13 +1,41 @@
 #include "Sprite.hh"
 
-Sprite::Sprite(short x, short y, unsigned short width, unsigned short height) :
+using namespace std;
+
+Sprite::Sprite(string filename, short x, short y, 
+               unsigned short width, unsigned short height) :
   x_(x),
   y_(y),
   width_(width),
-  height_(height)
+  height_(height),
+  filename_(filename)
+{}
+
+Sprite::Sprite(const Sprite &other) :
+  x_(other.x_),
+  y_(other.y_),
+  width_(other.width_),
+  height_(other.height_),
+  filename_(other.filename_)
 {}
 
 Sprite::~Sprite() {}
+
+Sprite &Sprite::operator=(const Sprite &other)
+{
+  if (this != &other)
+  {
+    Sprite clone(other);
+    *this = clone;
+  }
+
+  return *this;
+}
+
+const std::string &Sprite::filename() const
+{
+  return this->filename_;
+}
 
 short Sprite::x() const
 {
--- src/Sprite.hh
+++ src/Sprite.hh
@@ -3,29 +3,32 @@
 #ifndef __SPRITE_HH__
 #define __SPRITE_HH__
 
+#include <string>
+
 class Sprite
 {
 public:
-    Sprite(short x, short y, unsigned short width, unsigned short height);
+    Sprite(std::string filename, short x, short y, 
+           unsigned short width, unsigned short height);
+    Sprite(const Sprite &other);
     virtual ~Sprite();
 
+    virtual Sprite &operator=(const Sprite &other);
+
+    const std::string &filename() const;
     short x() const;
     short y() const;
     unsigned short width() const;
     unsigned short height() const;
 
-    
 protected:
     short x_;
     short y_;
-    unsigned short width_;
-    unsigned short height_;
-
+    const unsigned short width_;
+    const unsigned short height_;
 
 private:
-    Sprite(const Sprite&);
-    void operator=(const Sprite&);
-
+    const std::string filename_;
 };
 
 #endif //__SPRITE_HH__
